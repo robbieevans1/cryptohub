e30c93bd94c1055409c8ac62b94ebd38c3e21cfc
tuckl <tuckerlarrabee13@gmail.com>
2022-03-15 23:27:16 -0400

    buttons persistent and button links redirecting correctly

--- public/javascript/favorites.js
+++ public/javascript/favorites.js
@@ -1,11 +1,20 @@
-// const e = require("express");
 let id;
+
 let cryptoButtonContainer = document.querySelector('#CryptoSearchedContainer')
 
 let newButtonNav = function specificCoinNav () {
-    
+        
     document.location.replace(`/specific-coin/${id}`);
-}
+    }
+
+let newButtonNavStorage = function specificCoinNav (e) {
+        console.log(e.currentTarget.myParam)
+    document.location.replace(`/specific-coin/${e.currentTarget.myParam}`);
+    }
+
+
+
+
 
 function favoriteHandler () {
     let coinFavorite = (this.closest('.card-body'))
@@ -14,7 +23,7 @@ function favoriteHandler () {
     id = coinFavorite.children[2].id;
     // let id 
 
-    // localStorage.setItem(JSON.stringify(coinFavorite), JSON.stringify(coinFavorite));
+    localStorage.setItem(JSON.stringify({coinText, id}), JSON.stringify({coinText, id}));
 
     let coinButtonEl = document.createElement('button');
     coinButtonEl.textContent = coinText;
@@ -27,7 +36,26 @@ function favoriteHandler () {
 };
 
 // coinButtonEl.addEventListener('click', specificCoinNav)
+function loadButtons () {
+    // if (localStorage.length !== 0) {
+    //     searchContainerEl.classList.remove('is-invisible');
+    // }
 
+    for (let i = 0; i < localStorage.length; i++) {
+    let coinButton = document.createElement('button');
+    let storedCoins = JSON.parse(localStorage.getItem(localStorage.key(i)));
+
+    console.log(storedCoins)
+    coinButton.textContent = storedCoins.coinText;
+    coinButton.classList.add("searchedButtons");
+    coinButton.addEventListener('click', newButtonNavStorage, false)
+    coinButton.myParam = storedCoins.id
+    cryptoButtonContainer.appendChild(coinButton);
+
+    // id = storedCoins.id;
+    // coinButton.id = id;
+}
+}
 
 
 
@@ -37,3 +65,5 @@ heart.forEach(heart => {
     heart.children[2].addEventListener('click', favoriteHandler)
 })
 // heart.addEventListener('click', favoriteHandler);
+
+loadButtons();
